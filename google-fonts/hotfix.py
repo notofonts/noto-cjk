727f898acdf7b100d308af8edf63c3953b626a1b
Simon Cozens <simon@simon-cozens.org>
2023-08-31 16:11:56 +0100

    Serif 2.002 hotfix 1

--- google-fonts/hotfix.py
+++ google-fonts/hotfix.py
@@ -27,7 +27,7 @@ import argparse
 import re
 import os
 
-HOTFIX_NUMBER = 2
+HOTFIX_NUMBER = 1
 
 OFL_DESCRIPTION = "This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL"
 
