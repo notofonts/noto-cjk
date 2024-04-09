5b1d953b6caebd90d4d02b964bf3728f1cc28f8d
Simon Cozens <simon@simon-cozens.org>
2024-04-09 13:04:02 +0100

    Build TTCs

--- build-gf.sh
+++ build-gf.sh
@@ -4,3 +4,7 @@ SANS_SOURCES=Sans/Variable/TTF/Subset/*ttf
 
 python3 google-fonts/hotfix.py --output-dir=google-fonts $SERIF_SOURCES $SANS_SOURCES
 add-chws -o google-fonts google-fonts/*ttf
+
+# Build TTCs for Android
+fonttools ttLib -o google-fonts/'NotoSansCJK[wght].ttf.ttc' google-fonts/NotoSans*.ttf
+fonttools ttLib -o google-fonts/'NotoSerifCJK[wght].ttf.ttc' google-fonts/NotoSerif*.ttf
